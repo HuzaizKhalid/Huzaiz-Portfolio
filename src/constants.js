82507355e786d9b24b262c2005240128db566330
Huzaiz Khalid <huzaizwebdev@gmail.com>
2025-06-24 22:53:00 +0500

    first commit

---
+++ src/constants.js
@@ -0,0 +1,254 @@
+// Skills Section Logo's
+import htmlLogo from "./assets/tech_logo/html.png";
+import cssLogo from "./assets/tech_logo/css.png";
+import sassLogo from "./assets/tech_logo/sass.png";
+import javascriptLogo from "./assets/tech_logo/javascript.png";
+import reactjsLogo from "./assets/tech_logo/reactjs.png";
+import angularLogo from "./assets/tech_logo/angular.png";
+import reduxLogo from "./assets/tech_logo/redux.png";
+import nextjsLogo from "./assets/tech_logo/nextjs.png";
+import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
+import gsapLogo from "./assets/tech_logo/gsap.png";
+import materialuiLogo from "./assets/tech_logo/materialui.png";
+import bootstrapLogo from "./assets/tech_logo/bootstrap.png";
+import springbootLogo from "./assets/tech_logo/springboot.png";
+import nodejsLogo from "./assets/tech_logo/nodejs.png";
+import expressjsLogo from "./assets/tech_logo/express.png";
+import mysqlLogo from "./assets/tech_logo/mysql.png";
+import mongodbLogo from "./assets/tech_logo/mongodb.png";
+import firebaseLogo from "./assets/tech_logo/firebase.png";
+import cLogo from "./assets/tech_logo/c.png";
+import cppLogo from "./assets/tech_logo/cpp.png";
+import javaLogo from "./assets/tech_logo/java.png";
+import pythonLogo from "./assets/tech_logo/python.png";
+import typescriptLogo from "./assets/tech_logo/typescript.png";
+import gitLogo from "./assets/tech_logo/git.png";
+import githubLogo from "./assets/tech_logo/github.png";
+import vscodeLogo from "./assets/tech_logo/vscode.png";
+import postmanLogo from "./assets/tech_logo/postman.png";
+import mcLogo from "./assets/tech_logo/mc.png";
+import figmaLogo from "./assets/tech_logo/figma.png";
+import netlifyLogo from "./assets/tech_logo/netlify.png";
+import vercelLogo from "./assets/tech_logo/vercel.png";
+import postgreLogo from "./assets/tech_logo/postgre.png";
+import csharpLogo from "./assets/tech_logo/csharp.png";
+
+// Experience Section Logo's
+import webverseLogo from "./assets/company_logo/webverse_logo.png";
+import agcLogo from "./assets/company_logo/agc_logo.png";
+import newtonschoolLogo from "./assets/company_logo/newtonschool_logo.png";
+
+// Education Section Logo's
+import PIEAS from "./assets/education_logo/PIEAS.png";
+import PGC from "./assets/education_logo/PGC.png";
+import vpsLogo from "./assets/education_logo/vps_logo.png";
+
+// Project Section Logo's
+import githubdetLogo from "./assets/work_logo/github_det.png";
+import animated_portfolio from "./assets/work_logo/animated_portfolio.png";
+import movierecLogo from "./assets/work_logo/movie_rec.png";
+import taskremLogo from "./assets/work_logo/task_rem.png";
+import npmLogo from "./assets/work_logo/npm.png";
+import webverLogo from "./assets/work_logo/web_dig.png";
+import careerConnect from "./assets/work_logo/careerConnect.png";
+import imagesearchLogo from "./assets/work_logo/image_search.png";
+import coderGirl from "./assets/work_logo/coderGirl.png";
+
+export const SkillsInfo = [
+  {
+    title: "Frontend",
+    skills: [
+      { name: "HTML", logo: htmlLogo },
+      { name: "CSS", logo: cssLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "React JS", logo: reactjsLogo },
+      { name: "Redux", logo: reduxLogo },
+      { name: "Tailwind CSS", logo: tailwindcssLogo },
+      { name: "Bootstrap", logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      { name: "Node JS", logo: nodejsLogo },
+      { name: "Express JS", logo: expressjsLogo },
+      { name: "MySQL", logo: mysqlLogo },
+      { name: "MongoDB", logo: mongodbLogo },
+      { name: "Firebase", logo: firebaseLogo },
+      { name: "PostgreSQL", logo: postgreLogo },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      { name: "Git", logo: gitLogo },
+      { name: "GitHub", logo: githubLogo },
+      { name: "VS Code", logo: vscodeLogo },
+      { name: "Postman", logo: postmanLogo },
+      { name: "Compass", logo: mcLogo },
+      { name: "Vercel", logo: vercelLogo },
+      { name: "Netlify", logo: netlifyLogo },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      { name: "C", logo: cLogo },
+      { name: "C++", logo: cppLogo },
+      { name: "Python", logo: pythonLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    // img: webverseLogo,
+    role: "Fullstack Developer Intern",
+    company: "Disruptive AI",
+    date: "Sep 2024 - Nov 2024",
+    desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+      "TypeScript",
+      "Node JS",
+      "Tailwind CSS",
+      "MongoDb",
+      "Redux",
+      " Next Js",
+    ],
+  },
+  {
+    id: 1,
+    // img: agcLogo,
+    role: "MERN Stack Fellow",
+    company: "Byetwise Limited",
+    date: "May 2024 - June 2024",
+    desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, React.js, Node.js, Express.js, and Mongodb. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    skills: ["ReactJS", "Redux", "JavaScript", "Tailwind CSS", "HTML", "CSS"],
+  },
+  {
+    id: 2,
+    // img: newtonschoolLogo,
+    role: "Freelancer",
+    company: "Fiverr Upwork",
+    date: "June 2022 - Present",
+    desc: "Worked with over 60+ projects of over 45+ unique clients over the past 2 years with the outstanding rating of 4.8 on Fiverr. Have secured 5 jobs on Upwork this year with outstanding 100% success score.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Bootstrap",
+      "Figma",
+      "Tailwind CSS",
+      " React JS",
+      "Node JS",
+      "Express JS",
+      "MongoDB",
+      "MySQL",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: PIEAS,
+    school: "PIEAS University, Islamabad",
+    date: "October 2021 - June 2025",
+    // grade: "7.81 CGPA",
+    desc: "I have completed my Bachelor's degree (MCA) in Computer Science from PIEAS University, Mathura. During my studies, I gained a solid foundation in computer science principles and practices. My coursework included advanced topics such as Data Structures, Algorithms, Web Development, Database Management Systems, and Software Engineering. I also had the opportunity to work on various projects that allowed me to apply theoretical knowledge to practical scenarios, enhancing my problem-solving skills and technical expertise.",
+    degree: "Bachelor's of Computer Science - BSCS",
+  },
+  {
+    id: 1,
+    img: PGC,
+    school: "Punjab College of Science, Multan",
+    date: "June 2019 - May 2021",
+    grade: "73.2%",
+    desc: "I completed my Intermediate in Pre Engineering (FSc.) from Punjab College, Multan. My studies included a strong focus on Mathematics and Physics, which laid the groundwork for my future studies in Computer Science.",
+    degree: "Intermediate - FSC (Pre Engineering)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "GitHub Profile Detective",
+    description:
+      "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+    image: githubdetLogo,
+    tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+    github:
+      "https://https://github.com/HuzaizKhalid/GitHub-Profile-Search-App-Using-React-JS",
+    webapp: "https://https://git-hub-profile-search-app-using-re.vercel.app/",
+  },
+  {
+    id: 1,
+    title: "Animated Portfolio Website",
+    description:
+      "A visually appealing and interactive portfolio website built with HTML, CSS and JavaScript.The website is fully responsive, ensuring a great user experience across all devices.",
+    image: animated_portfolio,
+    tags: ["HTML", "CSS", "JavaScript"],
+    github:
+      "https://https://github.com/HuzaizKhalid/animated-Portfolio-website-html-css-js",
+    webapp: "https://animated-portfolio-website-html-css.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Movie Recommendation App",
+    description:
+      "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+    image: movierecLogo,
+    tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+    github: "https://github.com/HuzaizKhalid/Movie-Web-App",
+    webapp: "https://movie-web-app-blue-omega.vercel.app  ",
+  },
+
+  {
+    id: 3,
+    title: "Career Connect",
+    description:
+      "The final-year project at PIEAS focused on developing a career-focused social media web application, connecting job seekers and recruiters. Built using the MERN stack (MongoDB, Express.js, React.js, Node.js), the platform features secure user authentication, profile building, post and job creation, job applications with resume uploads, real-time messaging (Socket.io), and user networking via follow/unfollow. It also integrates an AI-powered job recommendation engine using Pinecone and Gemini (RAG), ensuring personalized job matching based on user profiles. Cloudinary was used for media storage, ensuring a smooth and scalable user experience.",
+    image: careerConnect,
+    tags: [
+      "React JS",
+      "Node.js",
+      "MongoDB",
+      "Express",
+      "RAG",
+      "AI",
+      "Socket.io",
+      "Tailwind CSS",
+      "Cloudinary",
+      "Pinecone",
+      "Gemini",
+    ],
+    github: "https://github.com/HuzaizKhalid/Career-Connect/",
+    webapp: "https://github.com/HuzaizKhalid/Career-Connect/",
+  },
+  {
+    id: 4,
+    title: "Image Search App",
+    description:
+      "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    image: imagesearchLogo,
+    tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    github: "https://https://github.com/HuzaizKhalid/ImageSearchApp",
+    webapp: "https://image-search-app-blue-eta.vercel.app",
+  },
+  {
+    id: 5,
+    title: "Stunning Landing Page",
+    description:
+      "A visually appealing and interactive landing page built with HTML, CSS, JS. ",
+    image: coderGirl,
+    tags: ["HTML", "CSS", "Javascript"],
+    github: "https://github.com/HuzaizKhalid/CoderGirl-Landing-Page",
+    webapp: "https://https://coder-girl-landing-page.vercel.app/#",
+  },
+];
