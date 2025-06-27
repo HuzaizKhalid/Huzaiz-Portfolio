ced5885ca095789c321ac67b04661c582045d7ef
Huzaiz Khalid <huzaizwebdev@gmail.com>
2025-06-27 20:33:01 +0500

    Made CV link downloadable from Google Drive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/18isXyYwgtxi3tuP8POq9V1najzuHcaMH/view?usp=drive_link"
+            href="https://drive.google.com/uc?export=download&id=1Zu92OHBtYDoxnxdkKiWfsprskancVNSF"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
